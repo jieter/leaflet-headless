1f0a5d46493f8dd8b5d9d44ec2598f2ed3606f7a
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-12-18 12:16:32 +0100

    Factor out image shim implementation, use local leaflet

--- index.js
+++ index.js
@@ -6,10 +6,6 @@
 
 'use strict';
 
-var fs = require('fs');
-var request = require('request').defaults({
-	encoding: null
-});
 var jsdom = require('jsdom').jsdom;
 
 // make some globals to fake browser behaviour.
@@ -17,58 +13,19 @@ GLOBAL.document = jsdom('<html><head></head><body></body></html>');
 GLOBAL.window = document.parentWindow;
 GLOBAL.window.navigator.userAgent = 'webkit';
 GLOBAL.navigator = GLOBAL.window.navigator;
+GLOBAL.Image = require('./src/image.js');
 
-// shim Image
-var canvasImage = require('canvas').Image;
-var imageShim = function imageShim() {};
-imageShim.prototype.__defineSetter__('src', function (src) {
-	var self = this;
-
-	function buffer2image(buffer) {
-		var image = new canvasImage();
-		image.src = buffer;
-
-		if (self.onload) {
-			self.onload.apply(image);
-		}
-	}
-	switch (src.substr(0, 7)) {
-	case 'http://':
-		request.get(src, function (err, res, buffer) {
-			buffer2image(buffer);
-		});
-		break;
-	case 'file://':
-		// remove file://
-		src = src.substr(7);
-		// and query string
-		if (src.indexOf('?') !== -1) {
-			src = src.substr(0, src.indexOf('?'));
-		}
-
-		fs.readFile(src, function (err, buffer) {
-			buffer2image(buffer);
-		});
-		break;
-	default:
-		console.error('what to do? ' + src);
-	}
-});
-
-GLOBAL.Image = imageShim;
+var leafletSrc = './lib/leaflet/leaflet-src.js' || require.resolve('leaflet');
 
 GLOBAL.L_DISABLE_3D = true;
-
-var L = require('leaflet');
-
-// make L global too
+var L = require(leafletSrc);
 GLOBAL.L = L;
 
-var leafletPath = require.resolve('leaflet');
-L.Icon.Default.imagePath = leafletPath.substring(0, leafletPath.length - 10) + 'images';
+
+L.Icon.Default.imagePath = leafletSrc.substring(0, leafletSrc.length - 10) + 'images';
 
 // monkeypatch map.getSize to make it work with fixed 1024x1024 elements
-// jsdom appears to not have clientHeight/clientWidth on elements
+// jsdom does not have clientHeight/clientWidth on elements
 L.Map.prototype.getSize = function () {
 	return L.point(1024, 1024);
 };
